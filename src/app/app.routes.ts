afa3cc3d90622b92309f301712aa6d597b97bcee
ManuelAlrod <manuelalmengor32@gmail.com>
2024-05-01 23:00:22 -0500

    add sign up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,4 +14,18 @@ export const routes: Routes = [
     path: 'auth',
     loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
   },
+  {
+    path: 'auth-sign-up',
+    loadComponent: () => import('./auth-sign-up/auth-sign-up.page').then( m => m.AuthSignUpPage)
+  },
+  {
+    path: 'auth-options',
+    loadComponent: () => import('./auth-options/auth-options.page').then( m => m.AuthOptionsPage)
+  },
+
+  {
+    path: 'profile',
+    loadComponent: () => import('./profile/profile.page').then( m => m.ProfilePage)
+  }
+
 ];
