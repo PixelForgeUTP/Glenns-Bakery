573f05ba7b94d1a683174de9df64df80b268a208
Gi0Gio <zerokay02@gmail.com>
2024-05-01 23:27:26 -0500

    Menu edditing

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent, IonList, IonAvatar, IonLabel, IonItem, IonButton, IonButtons, IonMenuButton} from '@ionic/angular/standalone';
+import { 
+  IonContent, IonHeader, IonTitle, 
+  IonToolbar, IonCard, IonCardContent, 
+  IonList, IonAvatar, IonLabel, IonItem, 
+  IonButton, IonButtons, IonMenuButton} from '@ionic/angular/standalone';
+
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonButton, IonItem, IonLabel, IonAvatar, IonList, IonCardContent, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton]
+  imports: [
+    IonButtons,
+    MenuComponent,
+    IonButton, 
+    IonItem, 
+    IonLabel, 
+    IonAvatar, 
+    IonList, 
+    IonCardContent, 
+    IonCard, 
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule, 
+    FormsModule, 
+    IonMenuButton]
 })
 export class ProfilePage implements OnInit {
 
