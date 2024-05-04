b681b6c6c60c096fc0f207232c364dbadf7e7997
ManuelAlrod <manuelalmengor32@gmail.com>
2024-05-04 16:23:42 -0500

    add styles

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonItem, IonLabel, IonMenu, IonMenuButton, IonButtons, IonList, IonInput} from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonButton, 
+  IonItem, 
+  IonLabel, 
+  IonMenu, 
+  IonMenuButton, 
+  IonButtons, 
+  IonList, 
+  IonInput
+} from '@ionic/angular/standalone';
 
 import { CustomInputComponent } from "../components/custom-input/custom-input.component";
 @Component({
@@ -42,7 +55,11 @@ export class AuthPage implements OnInit {
     console.log(value);
   }
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private location:Location,
+  ) { }
 
   onSubmit() {
     console.log('Form Submitted', this.loginForm.value);
@@ -53,6 +70,7 @@ export class AuthPage implements OnInit {
         .then(result => {
           console.log("You are successfully logged in!", result);
           // Navigate to the desired route upon successful login
+          this.router.navigate(['/home']);
         })
         .catch(error => {
           console.error("Error logging in: ", error);
@@ -67,6 +85,11 @@ export class AuthPage implements OnInit {
       });
     }
   }
+
+  goBack() {
+    this.location.back();
+  }
+
   ngOnInit() {
   }
 
